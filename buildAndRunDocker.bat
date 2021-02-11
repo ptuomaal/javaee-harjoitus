4071657078a39f0df5cd147589f0a7c4bb8c94a8
Henri Aho <dankirk_88@msn.com>
2021-02-11 09:09:46 +0200

    Korjauttu .bat skripti

--- buildAndRunDocker.bat
+++ buildAndRunDocker.bat
@@ -1,5 +1,5 @@
 
-mvn clean package 
-docker build -t fi.ptuomaal/ping .
-docker rm -f ping 
-docker run -d -p 9990:9990 -p 8080:8080 -p 4848:4848 --name ping fi.ptuomaal/ping
+call mvn clean package 
+call docker build -t fi.ptuomaal/ping .
+call docker rm -f ping 
+call docker run -d -p 9990:9990 -p 8080:8080 -p 4848:4848 --name ping fi.ptuomaal/ping
