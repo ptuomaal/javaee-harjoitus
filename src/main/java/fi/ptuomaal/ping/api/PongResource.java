ce7c464727cf47e3a0891c11570524706f2ba239
Sami Tiihonen <stiihonen@gmail.com>
2021-11-28 18:00:19 +0200

    palauta /ping/resources/ping/{nimi} text/html-muodossa

--- src/main/java/fi/ptuomaal/ping/api/PongResource.java
+++ src/main/java/fi/ptuomaal/ping/api/PongResource.java
@@ -12,6 +12,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import java.util.List;
 
 /**
@@ -25,9 +26,15 @@ public class PongResource {
 
     @GET
     @Path("/{name}")
-    @Produces(MediaType.APPLICATION_JSON)
-    public List<Pong> ping(@PathParam("name") String name) {
-        return service.getPongs(name);
+    @Produces(MediaType.TEXT_HTML)
+    public String ping(@PathParam("name") String name) {
+        List<Pong> pongs = service.getPongs(name);
+        String htmlList = "<ul>";
+        for (Pong p : pongs) {
+            htmlList += "<li>" + p.getId() + ": " + p.getName() + "</li>";
+        }
+        String page = "<html><body>" + htmlList + "</body></html>";
+        return page;
     }
 
     @GET
